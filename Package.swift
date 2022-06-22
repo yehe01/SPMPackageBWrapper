f50faf9a8a6ba10386d84152750a73ff30fa89b3
yehe01 <yehe01@gmail.com>
2022-06-22 22:37:40 +1000

    Add binary target.

--- Package.swift
+++ Package.swift
@@ -18,6 +18,11 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        
+        .binaryTarget(
+            name: "SPMPackageB",
+            url: "https://github.com/yehe01/SPMPackageB/files/8957526/SPMPackageB.xcframework.zip",
+            checksum: "f892d2868b9ea5731c6bc075f175dd0e54e72741f0e1ff5511213337a43c6de6"),
         .target(
             name: "SPMPackageBWrapper",
             dependencies: []),
